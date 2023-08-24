55ec065381e20a24b560ecc03ca860b71e441957
mogbonna <mogbonna045@gmail.com>
2023-08-24 06:43:37 -0700

    update task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,12 @@
 
 /**
  * premain - prints a sentence before the main
- * function is executed
+ * function is executed.
  */
-void __attribute__((constructor)) premain()
+__attribute__ ((constructor))
+void premain(void)
 {
-	printf("You're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
 
