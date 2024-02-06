6e6f02e3c50ff66c3d260e4a817db2acbb65e932
mogbonna <mogbonna045@gmail.com>
2024-02-06 13:18:52 -0800

    Jump search in a singly linked list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,14 +13,13 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
 	int n;
 	size_t index;
 	struct listint_s *next;
-} listint_t;
+}	listint_t;
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
